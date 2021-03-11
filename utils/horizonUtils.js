65a248d433e1b9ea5baab2a5b5312e62058517b6
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-11 13:59:43 -0300

    add mongoUri

--- utils/horizonUtils.js
+++ utils/horizonUtils.js
@@ -1,8 +1,9 @@
 let config;
 try {
-    config = require('../config.json');
+    config = require("../config.json");
 } catch (error) {
     config = null;
 }
 
 exports.discord = config ? config.discord : process.env.DISCORD;
+exports.mongoUri = config ? config.mongoUri : process.env.MONGO_URI;
